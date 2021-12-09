188667dc3ceabd4232aa305aa1e0ce771d9b4893
NikolayAI <kodi_kodi_kodi@mail.ru>
2021-12-09 21:24:20 +0500

    add github auth strategy

---
+++ backend/core/passport.ts
@@ -0,0 +1,1 @@
+import passport from 'passport';import { Strategy as GithubStrategy } from 'passport-github';passport.use(new GithubStrategy(  {    clientID: process.env.GITHUB_CLIENT_ID ?? '',    clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',    callbackURL: 'http://localhost:3001/auth/github/callback'  },  (accessToken, refreshToken, profile, cb) => {  }));
