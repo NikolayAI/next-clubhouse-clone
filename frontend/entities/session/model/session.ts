d1b8426cd5688f50526db18aff500058d2467fa9
NikolayAI <kodi_kodi_kodi@mail.ru>
2021-12-30 20:57:42 +0500

    add handle token on client

--- frontend/entities/session/model/session.ts
+++ frontend/entities/session/model/session.ts
@@ -1,1 +1,21 @@
-import { createEffect } from 'effector/compat';const authGitHubFx = createEffect<void, Window | null, Error>();authGitHubFx.use(() => {  return window.open(    'http://localhost:3001/auth/github',    'Auth',    'width=500,height=500,status=yes,toolbar=no,menubar=no,location=no',  );});export {  authGitHubFx,};
+import { createEffect } from 'effector/compat';
+import Cookies from 'js-cookie';
+
+import { IUserResponse } from '../../../../contracts';
+
+const authGitHubFx = createEffect<void, Window | null, Error>();
+const setCookieFx = createEffect<IUserResponse, void, Error>();
+
+authGitHubFx.use(() => {
+  return window.open(
+    'http://localhost:3001/auth/github',
+    'Auth',
+    'width=500,height=500,status=yes,toolbar=no,menubar=no,location=no'
+  );
+});
+
+setCookieFx.use(({ token = '' }) => {
+  Cookies.set('token', token);
+});
+
+export { authGitHubFx, setCookieFx };
